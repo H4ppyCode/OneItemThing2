47aa7e0193740efbf3c9c663d5e1031fe5fce3b6
Atreus <72207991+H4ppyCode@users.noreply.github.com>
2023-04-18 16:00:55 +0200

    Update app-routing.module.ts
    fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,13 @@ import { AuthenticatorComponent } from './pages/authenticator/authenticator.comp
 import { HistoryComponent } from './pages/history/history.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'authenticator', component: AuthenticatorComponent },
   { path: 'history', component: HistoryComponent },
   { path: 'home', component: HomeComponent },
   { path: 'item', component: ItemComponent },
+  { path: '', component: HomeComponent }
+ 
 ];
 
 @NgModule({
