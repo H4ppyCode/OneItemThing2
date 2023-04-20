05d4e716380298878ef32a9f199171170f418a51
Gaillard, Louis <gaillard.louis@courrier.uqam.ca>
2023-04-20 17:23:27 -0400

    Réparation du middle.directive.ts qui bug.

--- src/app/pages/item/middle.directive.ts
+++ src/app/pages/item/middle.directive.ts
@@ -1,26 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-selector: '[cardM]',
-host: {
-'style': display: flex; justify-content: center; align-items: center; }
+  selector: '[cardM]',
 })
-export class MiddleDirective {}
-
-//il y a des cas où vous devrez utiliser JavaScript pour configurer les styles, 
-// par exemple si vous devez configurer les styles dynamiquement en fonction de l'état de votre application. 
-// Dans ce cas, ce  code peut être plus approprié.
-// import { Directive, ElementRef, OnInit } from '@angular/core';
-
-// Directive({
-// selector: '[cardM]',
-// })
-// export class MiddleDirective implements OnInit {
-// constructor(private elementRef: ElementRef<HTMLDivElement>) {}
-
-// ngOnInit() {
-// this.elementRef.nativeElement.style.display = 'flex';
-// this.elementRef.nativeElement.style.justifyContent = 'center';
-// this.elementRef.nativeElement.style.alignItems = 'center';
-// }
-// }
+export class MiddleDirective {
+  constructor(private elementRef: ElementRef<HTMLDivElement>) {
+    this.elementRef.nativeElement.style.display = 'flex';
+    this.elementRef.nativeElement.style.justifyContent = 'center';
+    this.elementRef.nativeElement.style.alignItems = 'center';
+  }
+}
