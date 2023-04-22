fde8fdf73711145432a9d0d099ca2343f14f5518
Gaillard, Louis <gaillard.louis@courrier.uqam.ca>
2023-04-21 21:57:06 -0400

    Suivi du tuto Angular FireMan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,17 @@ import { HistoryComponent } from './pages/history/history.component';
 import { AboutComponent } from './pages/about/about.component';
 import { AuthenticatorComponent } from './pages/authenticator/authenticator.component';
 import { MiddleDirective } from './pages/item/middle.directive';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -24,8 +35,10 @@ import { MiddleDirective } from './pages/item/middle.directive';
     AuthenticatorComponent,
     MiddleDirective,
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAnalytics(() => getAnalytics()), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideRemoteConfig(() => getRemoteConfig()), provideStorage(() => getStorage())],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'oneitemback',
+    appId: '1:33379821613:web:1402345fa1b54f6c0370a4',
+    storageBucket: 'oneitemback.appspot.com',
+    apiKey: 'AIzaSyDP5B3uQpgx6TqHToh4HHBVpeZ4-trMuuc',
+    authDomain: 'oneitemback.firebaseapp.com',
+    messagingSenderId: '33379821613',
+  },};
