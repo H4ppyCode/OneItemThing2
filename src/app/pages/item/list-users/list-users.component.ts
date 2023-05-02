9403a4db068b08c62628742eb548844e90301129
H4ppycode <dhiser.pacome@gmail.com>
2023-05-02 15:17:32 +0200

    init

--- src/app/pages/item/list-users/list-users.component.ts
+++ src/app/pages/item/list-users/list-users.component.ts
@@ -6,10 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-users.component.css'],
 })
 export class ListUsersComponent {
-  items = [
-    { pseudo: 'PL', montant: '5€' },
-    { pseudo: 'Atreus', montant: '10M' },
-    { pseudo: 'Louis', montant: '20M' },
-    { pseudo: 'Row 4, Column 1', montant: 'Row 4,  2' },
-  ];
+  items: { pseudo: string; montant: string }[] = [];
+
+  constructor() {
+    const itemsFromStorage = localStorage.getItem('items');
+    // localStorage.clear(); ////Clear local storage
+    if (itemsFromStorage) {
+      this.items = JSON.parse(itemsFromStorage);
+    } else {
+      this.items = [
+        { pseudo: 'PL', montant: '5€' },
+        { pseudo: 'Atreus', montant: '10M' },
+      ];
+    }
+  }
+
+  ajouterUtilisateur(montantSaisi: string) {
+    const montant = parseInt(montantSaisi, 10);
+    if (isNaN(montant)) {
+      return;
+    }
+    const nouvelUtilisateur = { pseudo: 'test', montant: montantSaisi + '€' };
+    this.items.push(nouvelUtilisateur);
+    localStorage.setItem('items', JSON.stringify(this.items));
+  }
 }
